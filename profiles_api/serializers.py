720da05c35b90ea61d35a4dffa0f4f5455e93fb0
Sanjay Bhattacharya <sanjay.bhattacharya@aegeanllc.com>
2020-01-04 14:19:00 -0500

    Added Put, patch, Delete to views

---
+++ profiles_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+
+class HelloSerialzier (serializers.Serializer):
+    """Serializes a name field for testing APIview"""
+    name = serializers.CharField(max_length=10) 
